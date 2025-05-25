d26bd8fb1db08a2397131bb9115dda757b1a7af8
Kaorie-pg <147548786+Kaorie-pg@users.noreply.github.com>
2025-05-25 23:17:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+   res.render('index', {minimalNavbar: false});
+});
+
+app.get('/projets/runathon', (req, res) => {
+  res.render('projets/runathon', { minimalNavbar: true });
+});
+
+app.get('/projets/foreigngineer', (req, res) => {
+  res.render('projets/foreigngineer', { minimalNavbar: true });
+});
+
+app.get('/projets/glimmerwood', (req, res) => {
+  res.render('projets/glimmerwood', { minimalNavbar: true });
+});
+
+app.listen(port, () => {
+  console.log(`Portfolio en ligne sur http://localhost:${port}`);
+});
